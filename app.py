9191a1a7ed73f868d3aec7717467f9e2854ecf9a
Eli Propp <eli.propp@gmail.com>
2023-07-28 18:31:36 -0400

    add map pdf, get hebrew input

--- app.py
+++ app.py
@@ -3,6 +3,19 @@ import hebrew as hb
 
 def main():
     st.write("hello")
+    st.markdown("""
+        <style>
+        input {
+        unicode-bidi:bidi-override;
+        direction: RTL;
+        }
+        </style>
+            """, unsafe_allow_html=True)
+
+    input = st.text_input("Write the line in hebrew here:")
+    line = hb.Hebrew(input)
+    st.write(line)
+    # st.write('value 1: "', i1, '"')
 
 
 if __name__ == '__main__':
